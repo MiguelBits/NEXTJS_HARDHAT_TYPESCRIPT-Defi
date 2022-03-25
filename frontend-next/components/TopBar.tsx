837f63f3e0d2e5bf2945c5781b88ba9d68392328
mcruzvas <mcv.cooper@gmail.com>
2022-03-25 16:02:42 +0000

    added logo to top bar

--- frontend-next/components/TopBar.tsx
+++ frontend-next/components/TopBar.tsx
@@ -3,6 +3,7 @@ import styles from '../styles/Home.module.css'
 import { ethers } from 'ethers';
 import {tokenAddress, token2Address, tokenABI, factoryAddress, factoryABI, erc20ABI} from "./../pages/contracts_abi"
 import { FaEthereum } from "react-icons/fa";
+import Image from 'next/image'
 
 declare let window: any
 
@@ -168,6 +169,12 @@ export default class TopBar extends React.Component {
               Connect with MetaMask
             </button>
           </div>
+
+          <div className={styles.centerContainer}>
+            Dopex
+            <Image src="/../public/favicon.ico" width="40" height="20" alt="dope"></Image>
+          </div>
+
           <div className={styles.leftContainer}>
             <a href="/">
               <svg className=" mt-2 w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
