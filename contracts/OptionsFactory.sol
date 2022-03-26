283340c5e2c0badbfb71296caa4e9b2457027ebf
mcruzvas <mcv.cooper@gmail.com>
2022-03-26 00:36:07 +0000

    many options of same token implemented

--- contracts/OptionsFactory.sol
+++ contracts/OptionsFactory.sol
@@ -11,8 +11,8 @@ contract OptionsFactory {
     using SafeMath for uint;
 
     mapping(address => underlyingToken[]) public allTokens;    //all option tokens of underlyingtokens address
-    mapping(address => OptionToken) public allOptionTokens;
-    mapping(address => OptionToken) public allAntiOptionTokens;
+    mapping(address => OptionToken[]) public allOptionTokens;
+    mapping(address => OptionToken[]) public allAntiOptionTokens;
     mapping(address => address) public priceFeedOracle;     //price Oracle address of underlyingtokens address
 
     event createOptions(address indexed _underlyingToken);
@@ -45,21 +45,33 @@ contract OptionsFactory {
         //allTokens[_underlyingToken][0] => TOKEN 
         allTokens[_underlyingToken].push(underlyingToken(_underlyingToken));
 
-        allOptionTokens[_underlyingToken] = optionTokens;
+        allOptionTokens[_underlyingToken].push(optionTokens);
 
-        allAntiOptionTokens[_underlyingToken] = antiOptionTokens;
+        allAntiOptionTokens[_underlyingToken].push(antiOptionTokens);
 
         priceFeedOracle[_underlyingToken] = _priceFeed;
 
         emit createOptions(_underlyingToken);
     }
+    //create option not for the first time
+    function addOptionsToken(address _underlyingToken) onlyAdmin() public{
+        require(allTokens[_underlyingToken].length > 0, "Token does not exist in Factory");
 
+        OptionToken optionTokens = new OptionToken(address(this),_underlyingToken);
+        OptionToken antiOptionTokens = new OptionToken(address(this),_underlyingToken);
+
+        allOptionTokens[_underlyingToken].push(optionTokens);
+
+        allAntiOptionTokens[_underlyingToken].push(antiOptionTokens);
+
+        emit createOptions(_underlyingToken);
+    }
     //fund existing options and set strikes
-    function activateOption(address _underlyingToken, uint _strikePrice, uint _strikeDeadline, uint _amount, uint256 apy_ratio) public onlyAdmin(){
+    function activateOption(address _underlyingToken, uint _strikePrice, uint _strikeDeadline, uint _amount, uint256 apy_ratio, uint _orderNumber) public onlyAdmin(){
         require(allTokens[_underlyingToken].length > 0,"There are no options for this token");
         //get tokens
         underlyingToken my_underlyingToken = allTokens[_underlyingToken][0];
-        OptionToken option = allOptionTokens[_underlyingToken];
+        OptionToken option = allOptionTokens[_underlyingToken][_orderNumber];
         
         //factory funding and set strikes
         my_underlyingToken.transferFrom(msg.sender, address(this), _amount);
@@ -71,12 +83,12 @@ contract OptionsFactory {
         //mint Option Token
         option.mintOption(address(this), _amount);
     }
-    function activateAntiOption(address _underlyingToken, uint _strikePrice, uint _strikeDeadline, uint _amount, uint256 apy_ratio) public onlyAdmin(){
+    function activateAntiOption(address _underlyingToken, uint _strikePrice, uint _strikeDeadline, uint _amount, uint256 apy_ratio, uint _orderNumber) public onlyAdmin(){
         require(allTokens[_underlyingToken].length > 0,"There are no options for this token");
         //get tokens
         underlyingToken my_underlyingToken = allTokens[_underlyingToken][0];
 
-        OptionToken antiOption = allAntiOptionTokens[_underlyingToken];
+        OptionToken antiOption = allAntiOptionTokens[_underlyingToken][_orderNumber];
         //factory funding and set strikes
         my_underlyingToken.transferFrom(msg.sender, address(this), _amount);
         require(my_underlyingToken.balanceOf(address(this)) > 0, "Option does not have any amount of this Token");
@@ -89,13 +101,13 @@ contract OptionsFactory {
 
     }
     //fund existing options with already defined strikes
-    function fundOptions(address _underlyingToken, uint _amount) public onlyAdmin(){
+    function fundOptions(address _underlyingToken, uint _amount, uint _orderNumber) public onlyAdmin(){
         require(allTokens[_underlyingToken].length > 0,"There are no options for this token");
 
         //get tokens
         underlyingToken my_underlyingToken = allTokens[_underlyingToken][0];
-        OptionToken option = allOptionTokens[_underlyingToken];
-        OptionToken antiOption = allAntiOptionTokens[_underlyingToken];
+        OptionToken option = allOptionTokens[_underlyingToken][_orderNumber];
+        OptionToken antiOption = allAntiOptionTokens[_underlyingToken][_orderNumber];
 
         //factory funding and set strikes
         my_underlyingToken.transferFrom(msg.sender, address(this), _amount);
@@ -106,7 +118,7 @@ contract OptionsFactory {
         antiOption.mintOption(address(this), _amount);
     }
     //buy options and anti-option token
-    function buyOptions(address _underlyingToken, uint _amount) public payable{
+    function buyOptions(address _underlyingToken, uint _amount, uint _orderNumber) public payable{
         require(allTokens[_underlyingToken].length > 0,"There are no options for this token");
 
         //pay premium fee
@@ -117,7 +129,7 @@ contract OptionsFactory {
 
         //get Tokens
         underlyingToken my_underlyingToken = allTokens[_underlyingToken][0];
-        OptionToken option = allOptionTokens[_underlyingToken];
+        OptionToken option = allOptionTokens[_underlyingToken][_orderNumber];
 
         //approve
         option.approve(msg.sender,_amount);
@@ -132,7 +144,7 @@ contract OptionsFactory {
 
         emit buyOption(msg.sender, _amount);
     }
-    function buyAntiOptions(address _underlyingToken, uint _amount) public payable{
+    function buyAntiOptions(address _underlyingToken, uint _amount, uint _orderNumber) public payable{
         require(allTokens[_underlyingToken].length > 0,"There are no options for this token");
         //pay premium fee
         uint fee = _amount.div(1e18).mul(_feePolicy);
@@ -140,7 +152,7 @@ contract OptionsFactory {
 
         //get Tokens
         underlyingToken my_underlyingToken = allTokens[_underlyingToken][0];
-        OptionToken antiOption = allAntiOptionTokens[_underlyingToken];
+        OptionToken antiOption = allAntiOptionTokens[_underlyingToken][_orderNumber];
 
         //check factory underlyingToken balance
         require(my_underlyingToken.balanceOf(address(this)) >= _amount, "Not enough balance in Factory");
@@ -160,12 +172,12 @@ contract OptionsFactory {
     }
 
     //exercise options
-    function exerciseOption(address _underlyingToken, uint _amount) public payable{
+    function exerciseOption(address _underlyingToken, uint _amount, uint _orderNumber) public payable{
         require(allTokens[_underlyingToken].length > 0,"There are no options for this token");
 
         //get Tokens
         underlyingToken my_underlyingToken = allTokens[_underlyingToken][0];
-        OptionToken option = allOptionTokens[_underlyingToken];
+        OptionToken option = allOptionTokens[_underlyingToken][_orderNumber];
 
         require(option.balanceOf(msg.sender) >= _amount, "You do not have enough Option Tokens");
 
@@ -195,12 +207,12 @@ contract OptionsFactory {
 
         emit exercisedOption(_underlyingToken, _amount);
     }
-    function exerciseAntiOption(address _underlyingToken, uint _amount) public{
+    function exerciseAntiOption(address _underlyingToken, uint _amount, uint _orderNumber) public{
         require(allTokens[_underlyingToken].length > 0,"There are no options for this token");
 
         //get Tokens
         underlyingToken my_underlyingToken = allTokens[_underlyingToken][0];
-        OptionToken antiOption = allAntiOptionTokens[_underlyingToken];
+        OptionToken antiOption = allAntiOptionTokens[_underlyingToken][_orderNumber];
 
         require(antiOption.balanceOf(msg.sender) >= _amount, "You do not have enough Anti Option Tokens");
         //get Strikes
@@ -230,35 +242,35 @@ contract OptionsFactory {
         emit exercisedAntiOption(_underlyingToken, _amount);
     }
     //Getters
-    function getAmountOptions(address _underlyingToken, address _who) public view returns(uint){
+    function getAmountOptions(address _underlyingToken, address _who, uint _orderNumber) public view returns(uint){
         require(allTokens[_underlyingToken].length > 0,"There are no options for this token");
 
         //get Tokens
-        OptionToken option = allOptionTokens[_underlyingToken];
+        OptionToken option = allOptionTokens[_underlyingToken][_orderNumber];
         
         return option.balanceOf(_who);
     }
-    function getAmountAntiOptions(address _underlyingToken, address _who) public view returns(uint){
+    function getAmountAntiOptions(address _underlyingToken, address _who, uint _orderNumber) public view returns(uint){
         require(allTokens[_underlyingToken].length > 0,"There are no options for this token");
 
         //get Tokens
-        OptionToken antiOption = allAntiOptionTokens[_underlyingToken];
+        OptionToken antiOption = allAntiOptionTokens[_underlyingToken][_orderNumber];
         
         return antiOption.balanceOf(_who);
     }
     function getTokenBalance(address _token) public view returns(uint){
         return underlyingToken(_token).balanceOf(address(this));
     }
-    function getOptionApy(address _token) public view returns(uint){
+    function getOptionApy(address _token, uint _orderNumber) public view returns(uint){
         require(allTokens[_token].length > 0,"There are no options for this token");
         //get Tokens
-        OptionToken option = allOptionTokens[_token];
+        OptionToken option = allOptionTokens[_token][_orderNumber];
         return option.getApyRatio();
     }
-    function getAntiOptionApy(address _token) public view returns(uint){
+    function getAntiOptionApy(address _token, uint _orderNumber) public view returns(uint){
         require(allTokens[_token].length > 0,"There are no options for this token");
         //get Tokens
-        OptionToken antiOption = allAntiOptionTokens[_token];
+        OptionToken antiOption = allAntiOptionTokens[_token][_orderNumber];
         return antiOption.getApyRatio();
     }
     function getTVL(address _token) public view returns(uint){
@@ -267,41 +279,53 @@ contract OptionsFactory {
         underlyingToken my_underlyingToken = allTokens[_token][0];
         return my_underlyingToken.balanceOf(address(this));
     }
-    function getOptionStrikePrices(address _token) public view returns(uint){
+    function getOptionStrikePrices(address _token, uint _orderNumber) public view returns(uint){
         require(allTokens[_token].length > 0,"There are no options for this token");
         //get Tokens
-        OptionToken option = allOptionTokens[_token];
+        OptionToken option = allOptionTokens[_token][_orderNumber];
         return option.getStrikePrice();
     }
-    function getAntiOptionStrikePrices(address _token) public view returns(uint){
+    function getAntiOptionStrikePrices(address _token, uint _orderNumber) public view returns(uint){
         require(allTokens[_token].length > 0,"There are no options for this token");
         //get Tokens
-        OptionToken antiOption = allAntiOptionTokens[_token];
+        OptionToken antiOption = allAntiOptionTokens[_token][_orderNumber];
         return antiOption.getStrikePrice();
     }
-    function getOptionStrikeDeadline(address _token) public view returns(uint){
+    function getOptionStrikeDeadline(address _token, uint _orderNumber) public view returns(uint){
         require(allTokens[_token].length > 0,"There are no options for this token");
         //get Tokens
-        OptionToken option = allOptionTokens[_token];
+        OptionToken option = allOptionTokens[_token][_orderNumber];
         return option.getStrikeDeadline();
     }
-    function getAntiOptionStrikeDeadline(address _token) public view returns(uint){
+    function getAntiOptionStrikeDeadline(address _token, uint _orderNumber) public view returns(uint){
         require(allTokens[_token].length > 0,"There are no options for this token");
         //get Tokens
-        OptionToken antiOption = allAntiOptionTokens[_token];
+        OptionToken antiOption = allAntiOptionTokens[_token][_orderNumber];
         return antiOption.getStrikeDeadline();
     }
-    function getOptionAddress(address _token) public view returns(address){
+    function getOptionAddress(address _token, uint _orderNumber) public view returns(address){
         require(allTokens[_token].length > 0,"There are no options for this token");
         //get Tokens
-        OptionToken option = allOptionTokens[_token];
+        OptionToken option = allOptionTokens[_token][_orderNumber];
         return address(option);
     }
-    function getAntiOptionAddress(address _token) public view returns(address){
+    function getAntiOptionAddress(address _token, uint _orderNumber) public view returns(address){
         require(allTokens[_token].length > 0,"There are no options for this token");
         //get Tokens
-        OptionToken antiOption = allAntiOptionTokens[_token];
+        OptionToken antiOption = allAntiOptionTokens[_token][_orderNumber];
         return address(antiOption);
+    }
+    function getHowManyOptions(address _token) public view returns(uint){
+        require(allTokens[_token].length > 0,"There are no options for this token");
+        //get Tokens
+        uint option = allOptionTokens[_token].length;
+        return option;
+    }
+    function getHowManyAntiOptions(address _token) public view returns(uint){
+        require(allTokens[_token].length > 0,"There are no options for this token");
+        //get Tokens
+        uint antiOption = allAntiOptionTokens[_token].length;
+        return antiOption;
     }
     //withdraw all fees of ether
     function withdrawFees() public onlyAdmin(){
