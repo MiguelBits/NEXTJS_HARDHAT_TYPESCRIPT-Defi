e5e9cc2c72a444589c3cd3da408e91a2eff21531
mcruzvas <mcv.cooper@gmail.com>
2022-03-27 18:13:30 +0100

    reference new page

--- frontend-next/components/TopBar.tsx
+++ frontend-next/components/TopBar.tsx
@@ -201,8 +201,8 @@ export default class TopBar extends React.Component {
             </button>
           </div>
           <button className="absolute top-1 left-96 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-              <Link href="https://faucet.avax-test.network/" target="_blank" className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                  Mint AVAX
+              <Link href="https://faucet.avax-test.network/" >
+                  <a className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0" target="_blank">Mint AVAX</a>
               </Link>
             </button>
             
